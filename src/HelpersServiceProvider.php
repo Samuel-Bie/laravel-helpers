2be78aaa1019182f3388d9106a82003d18654df4
samuel-bie <samuelbiezinho@gmail.com>
2020-06-07 19:28:47 +0200

    Release of make TraitCommand

--- src/HelpersServiceProvider.php
+++ src/HelpersServiceProvider.php
@@ -3,19 +3,23 @@
 namespace samuelbie\helpers;
 
 use Illuminate\Support\ServiceProvider;
+use samuelbie\helpers\Console\TraitCommand;
 
 class HelpersServiceProvider extends ServiceProvider
 {
 
     public function boot()
     {
-
-
+        $this->registerCommands();
     }
 
     public function register()
     {
 
+    }
 
+    private function registerCommands()
+    {
+        $this->commands(TraitCommand::class);
     }
 }
